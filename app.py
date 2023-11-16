f3aaf6d652bf452232e08d2838e5e55aec88cb92
shivamsingh6200 <shivamsingh01012013@gmail.com>
2023-11-16 11:02:35 +0530

    Changed code for better look

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request
 import requests
+from flask import Flask, render_template, request
 
 app= Flask(__name__)
 
@@ -7,21 +7,26 @@ app= Flask(__name__)
 def homepage():
     return render_template("index.html")
 
-@app.route("/weatherapp",methods=['POST','GET'])
-def get_weatherdata():
+@app.route("/weather",methods=['POST','GET'])
+def searchpage():
     apikey="602e11c0c9bf90c661e13498979c2a9d"
     url="https://api.openweathermap.org/data/2.5/weather"
 
     para={"q":request.form.get("city"),
-     "units":"matric",
+     "units":"imperial",
      "appid":apikey
      }
     
     response=requests.get(url,params=para)
-    data=response.json()
-    city=data['name']
-    
-    return f"data : {data} , city : {city}"
+    data=response.json() 
+    weather = {
+            'city' :para["q"].upper(),
+            'temperature' :"%.1f" % float((data['main']['temp']-32)/1.8),
+            'description' : data['weather'][0]['description'],
+            'icon' : data['weather'][0]['icon'],
+        }
+    print (weather)
+    return render_template("weather.html",weather=weather)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0" , port = 5002)
